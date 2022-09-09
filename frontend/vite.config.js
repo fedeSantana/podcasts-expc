105e5a4f7eb9f02d75edd66f9318b256ed7f4230
fede.santana <fede.santana@hotmail.com>
2022-09-09 18:18:49 -0300

    fix: try to make it with other names

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,9 +10,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`
+        entryFileNames: `assets/[name]_podcasts.js`,
+        chunkFileNames: `assets/[name]_podcasts.js`,
+        assetFileNames: `assets/[name]_podcasts.[ext]`
       }
     }
   }
